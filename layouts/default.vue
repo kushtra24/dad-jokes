daf3caaaa9e4e08d1561346efcbe3da37bf347eb
kushtra24 <kushtra24@gmail.com>
2020-10-12 20:48:07 +0200

    search term, and limit filter

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,34 @@
   </div>
 </template>
 
+
+<script>
+  export default {
+    name: "edit",
+    head: {
+      title: process.env.npm_package_name || '',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      ],
+      script: [
+        { src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js' },
+        { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' },
+        { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        {
+          rel: 'stylesheet',
+          type: 'text/css',
+          href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css'
+        }
+      ]
+    },
+  }
+</script>
+
 <style>
 html {
   font-family:
--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,34 @@
     </div>
 
     <div class="jokes" v-show="!loading">
+
+      <div class="row">
+        <div class="col-lg-6 mb-2">
+        <form @submit.prevent="getJokes">
+            <div class="form-group row">
+              <input type="text" class="form-control col-10" v-model="term" placeholder="Search Jokes">
+              <button type="submit" class="btn btn-primary col-2">Search</button>
+            </div>
+          </form>
+        </div>
+        <div class="col-lg-6 p-0 mb-2">
+          <div class="btn-group float-right" >
+            <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Limit
+            </button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" @click="changeLimit(5)" href="#">5</a>
+              <a class="dropdown-item" @click="changeLimit(10)" href="#">10</a>
+              <a class="dropdown-item" @click="changeLimit(20)" href="#">20</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
      <div class="row">
+       <div class="alert alert-danger text-center" v-if="nothing" role="alert">
+         There are no Jokes with these conditions
+       </div>
        <div class="card col-md-4 col-sm-12" style="width: 18rem;" v-for="joke in jokes.results" :key="joke.id">
          <div class="card-body">
            <h5 class="card-title">{{ joke.id }}</h5>
@@ -43,46 +70,72 @@
     return {
       jokes: [],
       currentPage: 1,
-      perPage: 20,
+      limit: 20,
       totalPages: 20,
       pages: [],
-      loading: false
+      term: '',
+      loading: false,
+      nothing: false
     }
   },
 
     methods:{
      async getJokes() {
         this.loading = true;
-        let url = `http://localhost:8000/api/jokes?current_page=` + this.currentPage;
+
+        let url = 'http://localhost:8000/api/jokes?current_page=' + this.currentPage;
+
+        if (this.term) {
+          url += '&term=' + this.term
+        }
+
+        if (this.limit) {
+          url += '&limit=' + this.limit
+        }
+
         const jokes = await axios.get(url);
         this.jokes = jokes.data;
         this.totalPages = jokes.data.total_pages;
         this.currentPage = jokes.data.current_page;
-        this.currentPage = jokes.data.current_page;
+        this.limit = jokes.data.limit;
         this.loading = false;
+        if (this.jokes.results.length < 1) {
+          this.nothing = true;
+        }
         this.setPages();
       },
+
+      changeLimit(value) {
+       this.limit = value;
+       this.getJokes();
+      },
+
       nextPage() {
         this.currentPage++;
         this.getJokes()
       },
+
       prevPage() {
        this.currentPage--;
        this.getJokes();
       },
-      // setPage(pageNumber) {
-      //   this.currentPage = pageNumber;
-      //   this.getJokes();
-      // },
-      setPages () {
+
+      setPage(pageNumber) {
+        this.currentPage = pageNumber;
+        this.getJokes();
+      },
+
+      setPages() {
         for (let index = 1; index <= this.totalPages; index++) {
           this.pages.push(index);
         }
       },
     },
+
     created(){
       this.getJokes();
     },
+
 }
 </script>
 
